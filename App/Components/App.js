eff4ef29e5d56ce9fcdde85bb96c0ebe972d1ec4
luisaGonzales <luisagc711@gmail.com>
2018-12-18 17:46:29 -0500

    Fix 401 error, set client correctly

--- App/Components/App.js
+++ App/Components/App.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react';
-import Navbar from "./Navbar";
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from "apollo-boost";
+import {Text} from 'native-base';
+
+//Set Apollo Client
+const token = '<GITHUB_PERSONAL_ACCESS_TOKEN>';
+const client = new ApolloClient(
+  {
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  }
+  
+);
 
 export default class App extends Component {
   render() {
     return (
-      <Navbar />
+      <ApolloProvider client={client}>
+        <Text>Woks</Text>
+      </ApolloProvider>
     );
   }
 }
