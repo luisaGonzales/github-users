f7f6db70cdb3c560489fffb4dcfa6dac8725893d
luisaGonzales <luisagc711@gmail.com>
2018-12-19 12:14:08 -0500

    Styles on home screen
    Create separated file

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Text, Container, View, Item, Input, Icon} from 'native-base';
 import UsersList from '../Components/UsersListQuery'; 
+import styles from './Styles/HomeScreenStyles';
 
 class HomeScreen extends Component {
   static navigationOptions = {
@@ -15,20 +16,19 @@ class HomeScreen extends Component {
   render() {
     return (
       <Container>        
-        <View style={{backgroundColor: "#f9f9f9", paddingTop: 50, paddingBottom: 20, paddingHorizontal: 16, borderBottomWidth: 1, borderBottomColor: "#ececec" }}>
-          <Text style={{color: "#000", fontSize: 35, fontWeight: "900", marginBottom: 5}}>Github Users</Text>
+        <View style={styles.searchContainer}>
+          <Text style={styles.title}>Github Users</Text>
           <Item 
             rounded
-            style={{borderRadius: 12, padding: 0, height: 40, backgroundColor: "#ebebeb", borderColor: "transparent"}}
+            style={styles.inputContainer}
             >
-            <Icon active name='search' style={{fontSize: 20, paddingRight:0, color: "#949494" }} />
+            <Icon active name='search' style={styles.inputIcon} />
             <Input 
               placeholder='Search Github Users'
               placeholderTextColor="#949494"
               onChangeText={(text) => {
                 this.setState({text})
               }}
-              style={{padding: 0, placeholderTextColor: "red"}}
               />
           </Item>
         </View>
---
+++ App/Screens/Styles/HomeScreenStyles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  searchContainer: {
+    backgroundColor: "#f9f9f9", 
+    paddingTop: 50, 
+    paddingBottom: 20, 
+    paddingHorizontal: 16, 
+    borderBottomWidth: 1, 
+    borderBottomColor: "#ececec" 
+  },  
+  title: {
+    color: "#000", 
+    fontSize: 35, 
+    fontWeight: "900", 
+    marginBottom: 5
+  },
+  inputContainer: {
+    borderRadius: 12, 
+    padding: 0, 
+    height: 40, 
+    backgroundColor: "#ebebeb", 
+    borderColor: "transparent"
+  },
+  inputIcon: {
+    fontSize: 20, 
+    paddingRight:0, 
+    color: "#949494"
+  }
+})
