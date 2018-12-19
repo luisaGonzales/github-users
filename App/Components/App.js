9dd7166b302c7338fe568088c862682c0b8cd34d
luisaGonzales <luisagc711@gmail.com>
2018-12-19 09:14:41 -0500

    Fix navigation problem

--- App/Components/App.js
+++ App/Components/App.js
@@ -3,7 +3,7 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient from "apollo-boost";
 import {Text, Container} from 'native-base';
 
-import {createStackNavigator} from 'react-navigation';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 //Set Apollo Client
 const token = '<GITHUB_PERSONAL_ACCESS_TOKEN>';
@@ -19,6 +19,9 @@ const client = new ApolloClient(
 
 
 class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'GitHub Users',
+  };
   render() {
     return (
       <Container style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -28,17 +31,55 @@ class HomeScreen extends React.Component {
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {screen: HomeScreen},
-});
+class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('name'),
+    };
+  };
+  render() {
+    const { navigation } = this.props;
+    const itemId = navigation.getParam('id', 'NO-ID');
+    return (
+      <Container style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+        <Text>{itemId}</Text>
+      </Container>
+    );
+  }
+}
 
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Details: {
+    screen: DetailsScreen
+  }
+  },
+  {
+    initialRouteName: "Home",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#333',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
 
 
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <AppNavigator />
+        <AppContainer />
       </ApolloProvider>
     );
   }
