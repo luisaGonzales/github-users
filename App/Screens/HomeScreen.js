b71e9e305047781e1aa65b65520c9cc32e23b39a
luisaGonzales <luisagc711@gmail.com>
2018-12-19 09:25:18 -0500

    Get input and setup state

--- App/Screens/HomeScreen.js
+++ App/Screens/HomeScreen.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
-import {Text, Container} from 'native-base';
+import {Text, Container, Item, Input} from 'native-base';
 
 class HomeScreen extends Component {
   static navigationOptions = {
     title: 'GitHub Users',
   };
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
   render() {
     return (
-      <Container style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
+      <Container >        
+        <Text>Github Users</Text>
+        <Item rounded>
+          <Input 
+            placeholder='Search Github Users'
+            onChangeText={(text) => {
+              this.setState({text})
+            }}
+            />
+        </Item>
       </Container>
     );
   }
