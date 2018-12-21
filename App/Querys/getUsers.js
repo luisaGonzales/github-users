62d244358e0b57aaaa644998c03cfd511aafad95
luisaGonzales <luisagc711@gmail.com>
2018-12-21 09:49:27 -0500

    Fix bug
    Double export on query

--- App/Querys/getUsers.js
+++ App/Querys/getUsers.js
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
 
-export const GET_USERS = gql`
+const GET_USERS = gql`
   query SearchUsers($user: String!){
     search(query: $user, type: USER, first: 20) {
       edges {
@@ -18,20 +18,4 @@ export const GET_USERS = gql`
   }
 `;
 
-export const FETCH_USERS = gql`
-query SearchUsers($user: String!, $cursor: String){
-  search(query: $user, type: USER, first: 20, after: $cursor) {
-    edges {
-      cursor
-      node {
-        ... on User {
-          name
-          avatarUrl
-          location
-          login
-        }
-      }
-    }
-  }
-}
-`;
+export default GET_USERS;
