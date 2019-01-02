32f9afabdfb5586d2b34fb2d79c2faac9039ca95
luisaGonzales <luisagc711@gmail.com>
2019-01-02 12:29:17 -0500

    Add cache o fix heuristic data error

--- App/Components/App.js
+++ App/Components/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from 'react-apollo';
 import ApolloClient from "apollo-boost";
 import AppContainer from '../routes';
 import token from '../../config';
+import {InMemoryCache} from 'apollo-cache-inmemory'
 
 
 const client = new ApolloClient(
@@ -10,7 +11,14 @@ const client = new ApolloClient(
     uri: 'https://api.github.com/graphql',
     headers: {
       authorization: `Bearer ${token}`
-    }
+    },
+    cache: new InMemoryCache({
+      dataIdFromObject: obj => obj.id,
+      addTypename: false,
+      fragmentMatcher: {
+        match: ({ id }, typeCond, context) => !!context.store.get(id)
+      }
+    })
   }
   
 );
