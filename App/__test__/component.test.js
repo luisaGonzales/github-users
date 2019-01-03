e7cda2700c06fb306886880f0de28b699ac7cfc8
luisaGonzales <luisagc711@gmail.com>
2019-01-03 17:36:42 -0500

    Update testing

--- App/__test__/component.test.js
+++ App/__test__/component.test.js
@@ -5,10 +5,17 @@ import UserItem from '../Components/UserItem';
 import RepoItem from '../Components/RepoItem';
 import { ApolloProvider } from 'react-apollo';
 import { MockedProvider } from '../../node_modules/react-apollo/test-utils';
+import token from '../../config';
+import ApolloClient from "apollo-boost";
+import {InMemoryCache} from 'apollo-cache-inmemory'
 
 import UserList from '../Components/UsersListQuery';
+import RepoList from '../Components/ReposListQuery';
 import GET_USERS from '../Querys/getUsers';
+import GET_REPOS from '../Querys/getRepos';
+import 'isomorphic-fetch';
 
+//Testing render items
 test('Render user item with null data', () => {
   const userItemNull = renderer.create(
     <UserItem 
@@ -43,6 +50,63 @@ test('Render repo item', () => {
   ).toJSON();
   expect(repoItem).toMatchSnapshot();
 });
+
+//Testing data querys
+test('Get users query without errors', () => {
+ expect(GET_USERS).toMatchSnapshot();
+});
+
+test('Get repos query without errors', () => {
+  expect(GET_REPOS).toMatchSnapshot();
+});
+
+//Testing item properties
+test('Check properties ', () => {
+  expect(UserItem).toHaveProperty("name");
+});
+
+
+
+const client = new ApolloClient(
+  {
+    uri: 'https://api.github.com/graphql',
+    headers: {
+      authorization: `Bearer ${token}`
+    },
+    cache: new InMemoryCache({
+      dataIdFromObject: obj => obj.id,
+      addTypename: false,
+      fragmentMatcher: {
+        match: ({ id }, typeCond, context) => !!context.store.get(id)
+      }
+    })
+  }
+  
+);
+
+test('User list query', () => {
+  const userList = renderer.create(
+    <ApolloProvider client={ client } >  
+      <UserList user={"Luisa"}/>
+    </ ApolloProvider>
+  ).toJSON();
+
+  expect(userList).toMatchSnapshot()
+})
+
+test('Repo list query', () => {
+  const userList = renderer.create(
+    <ApolloProvider client={ client } >  
+      <RepoList login={"luisaGonzales"}/>
+    </ ApolloProvider>
+  ).toJSON();
+
+  expect(userList).toMatchSnapshot()
+})
+
+
+
+
 
 
 
